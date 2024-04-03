14ce688f4f5fd3841df46e065207483afa4bb7e1
Hermawan Purwanto <hermawanest@gmail.com>
2024-04-03 16:52:26 +0800

    insstruction dan draft code

---
+++ DigitalSignatureCheck/question.go
@@ -0,0 +1,56 @@
+package DigitalSignatureCheck
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"time"
+)
+
+// sha1sig return SHA1 signature in the format "35aabcd5a32e01d18a5ef688111624f3c547e13b"
+func sha1Sig(data []byte) (string, error) {
+	w := sha1.New()
+	r := bytes.NewReader(data)
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
+
+	sig := fmt.Sprintf("%x", w.Sum(nil))
+	return sig, nil
+}
+
+type File struct {
+	Name      string
+	Content   []byte
+	Signature string
+}
+
+// ValidateSigs return slice of OK files and slice of mismatched files
+func ValidateSigs(files []File) ([]string, []string, error) {
+	var okFiles []string
+	var badFiles []string
+
+	for _, file := range files {
+		sig, err := sha1Sig(file.Content)
+		if sig != file.Signature || err != nil {
+			badFiles = append(badFiles, file.Name)
+		} else {
+			okFiles = append(okFiles, file.Name)
+		}
+	}
+
+	return okFiles, badFiles, nil
+}
+
+func main() {
+	start := time.Now()
+
+	ok, bad, err := ValidateSigs(files)
+
+	duration := time.Since(start)
+	log.Printf("info: %d files in %v\n", len(ok)+len(bad), duration)
+	log.Printf("ok: %v", ok)
+	log.Printf("bad: %v", bad)
+}
